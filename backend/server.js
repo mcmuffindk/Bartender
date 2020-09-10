18d08e4e5b7219969c367e1593e8ff92fed96c3f
mcmuffindk <24530002+mcmuffindk@users.noreply.github.com>
2020-09-10 21:32:57 +0200

    Changed onoff to gpio

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const GPIO = require('onoff').Gpio
+const gpio = require('gpio');
 const log = require('loggy');
 const drinks = require('./static/drinks.json');
 const express = require('express');
@@ -8,6 +8,89 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 1337;
 
+// GPIO
+const relay1 = gpio.export(21, {
+   direction: gpio.DIRECTION.OUT,
+   ready: function() {
+   }
+});
+const relay2 = gpio.export(20, {
+   direction: gpio.DIRECTION.OUT,
+   ready: function() {
+   }
+});
+const relay3 = gpio.export(26, {
+   direction: gpio.DIRECTION.OUT,
+   ready: function() {
+   }
+});
+const relay4 = gpio.export(16, {
+   direction: gpio.DIRECTION.OUT,
+   ready: function() {
+   }
+});
+const relay5 = gpio.export(19, {
+   direction: gpio.DIRECTION.OUT,
+   ready: function() {
+   }
+});
+const relay6 = gpio.export(13, {
+   direction: gpio.DIRECTION.OUT,
+   ready: function() {
+   }
+});
+const relay7 = gpio.export(12, {
+   direction: gpio.DIRECTION.OUT,
+   ready: function() {
+   }
+});
+const relay8 = gpio.export(6, {
+   direction: gpio.DIRECTION.OUT,
+   ready: function() {
+   }
+});
+const relay9 = gpio.export(5, {
+   direction: gpio.DIRECTION.OUT,
+   ready: function() {
+   }
+});
+const relay10 = gpio.export(25, {
+   direction: gpio.DIRECTION.OUT,
+   ready: function() {
+   }
+});
+const relay11 = gpio.export(24, {
+   direction: gpio.DIRECTION.OUT,
+   ready: function() {
+   }
+});
+const relay12 = gpio.export(23, {
+   direction: gpio.DIRECTION.OUT,
+   ready: function() {
+   }
+});
+const relay13 = gpio.export(22, {
+   direction: gpio.DIRECTION.OUT,
+   ready: function() {
+   }
+});
+const relay14 = gpio.export(27, {
+   direction: gpio.DIRECTION.OUT,
+   ready: function() {
+   }
+});
+const relay15 = gpio.export(18, {
+   direction: gpio.DIRECTION.OUT,
+   ready: function() {
+   }
+});
+const relay16 = gpio.export(17, {
+   direction: gpio.DIRECTION.OUT,
+   ready: function() {
+   }
+});
+
+// ExpressJS
 app.use(express.static('static'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -17,13 +100,7 @@ app.get('/', (req, res) => {
 
 http.listen(port, log.info(`Bartender App is now running and listening on port ${port}`));
 
-
-
-
-
-
 // Socket.io
-
 io.on('connection', (socket) => {
   log.info('Kiosk connected'); // change
   socket.on('drink type', (drink) => {
