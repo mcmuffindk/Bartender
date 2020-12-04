5ab2178932703cca8ddb9d7532103c49211fb48f
mcmuffindk <24530002+mcmuffindk@users.noreply.github.com>
2020-12-04 08:58:31 +0100

    Fixed wrong data type

--- backend/config.js
+++ backend/config.js
@@ -23,22 +23,22 @@ const config = {
   port: 1337,
 
   // GPIO ports
-  relay1: 21,
-  relay2: 20,
-  relay3: 26,
-  relay4: 16,
-  relay5: 19,
-  relay6: 13,
-  relay7: 12,
-  relay8: 6,
-  relay9: 5,
-  relay10: 25,
-  relay11: 24,
-  relay12: 23,
-  relay13: 22,
-  relay14: 27,
-  relay15: 18,
-  relay16: 17
+  relay1: '21',
+  relay2: '20',
+  relay3: '26',
+  relay4: '16',
+  relay5: '19',
+  relay6: '13',
+  relay7: '12',
+  relay8: '6',
+  relay9: '5',
+  relay10: '25',
+  relay11: '24',
+  relay12: '23',
+  relay13: '22',
+  relay14: '27',
+  relay15: '18',
+  relay16: '17'
 };
 
 module.exports = config;
